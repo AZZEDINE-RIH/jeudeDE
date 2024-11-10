42975e818aaa5623a6030d0eca86dc7e21ae94e1
AZZEDINE-RIH <azzedinerih2001@gmail.com>
2024-11-10 19:12:24 +0100

    there is changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
      {/* <JeuDe /> */}
-     <JeuDEfunction />
+     <JeuDEfunction cache={2} />
     </div>
   );
 }
--- src/JeuDe.js
+++ src/JeuDe.js
@@ -6,20 +6,22 @@ export default class JeuDe extends React.Component {
     this.state = { face: null, compteur: 0, fin: false };
   }
 
+  // Fonction pour jouer
   jouer() {
     const valeur = Math.floor(Math.random() * 6) + 1;
-    const C = this.state.compteur + 1;
-    this.setState({
+    this.setState((prevState) => ({
       face: valeur,
-      compteur: C,
-      fin: valeur === this.props.cache
-    });
+      compteur: prevState.compteur + 1,
+      fin: valeur === this.props.cache, // Vérifie si le dé est égal à la face cachée
+    }));
   }
 
+  // Méthode pour obtenir l'image correspondante à la face du dé
   getImage() {
-    return this.state.face ? `images/face${this.state.face}.png` : "images/Dé.PNG";
+    return `images/face${this.state.face}.png`; // Retourné selon la face du dé
   }
 
+  // Fonction pour réinitialiser l'état
   initialiser() {
     this.setState({ face: null, compteur: 0, fin: false });
   }
@@ -28,12 +30,17 @@ export default class JeuDe extends React.Component {
     const styleImage = { width: "60px", height: "60px" };
     return (
       <div>
+        <img src="images/Dé.PNG" alt="Dé" />
         <h1>Jeu de Dé...</h1>
-        <img src={this.getImage()} style={styleImage} alt="Face du dé" />
-        <h2>Face: {this.state.face || "..."}</h2>
+        <h2>Face: {this.state.face}</h2>
+        {/* Affichage de l'image de la face du dé */}
+        {this.state.face && <img src={this.getImage()} alt={`Face ${this.state.face}`} style={styleImage} />}
         <h2>Nombre d'essais: {this.state.compteur}</h2>
         <button onClick={() => this.jouer()}>Jouer</button>
-        {this.state.fin && <p>Bravo! Vous avez trouvé la face cachée {this.props.cache}!</p>}
+
+        {/* Affichage du message de victoire si la face cachée est trouvée */}
+        {this.state.fin && <p>Bravo, vous avez trouvé la face cachée!</p>}
+
         <button onClick={() => this.initialiser()}>Initialiser</button>
       </div>
     );
